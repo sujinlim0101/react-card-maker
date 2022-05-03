d5bf6ea6b8a28499679a2c56e5f9b8a53427b622
sujin lim <a01041401160@gmail.com>
2022-05-03 22:48:31 +0900

    Add card maker

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React, { memo } from 'react';
+import ReactDOM from 'react-dom';
+import './index.module.css';
+import App from './app';
+import AuthService from './service/auth_service';
+import ImageUploader from './service/image_uploader';
+import ImageFileInput from './components/image_file_input/image_file_input';
+import CardRepository from './service/card_repository';
+import { firebaseApp } from './service/firebase';
+
+const authService = new AuthService(firebaseApp);
+const cardRepository = new CardRepository(firebaseApp);
+const imageUploader = new ImageUploader();
+const FileInput = memo((props) => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App
+      authService={authService}
+      FileInput={FileInput}
+      cardRepository={cardRepository}
+    />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
